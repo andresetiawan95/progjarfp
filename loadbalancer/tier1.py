ad3cae877b05f2e560268e29f3dcb18d860ef4c8
Andre Setiawan <Andre Setiawan>
2015-12-22 13:50:13 +0700

    tier 1 load balancer revisi

--- loadbalancer/tier1.py
+++ loadbalancer/tier1.py
@@ -9,19 +9,20 @@ port = 8580
 tier2 = []
 for x in range(5):
 	tier2.append (webserveraddr + ":%d"%(8581+x))
-
+	
+print tier2
 
 
 class Server(tierhandler):
 	def do_GET(self):
 		self.send_response(302)
-		web_path = 'http://%s:%s'%(tier2[ip],tier2[ip+1])
-		ip+=2
+		web_path = 'http://%s'%(tier2[ip])
+		ip=ip+1
 		if (ip >= len(tier2)):
 			ip=0
 		self.send_header('Location', web_path)
 		self.end_headers()
 
 SocketServer.TCPServer.allow_reuse_address = True
-print "load balancer berjalan di ip %s port %d" % (webserveraddr,port)
+print "load balancer tier1 berjalan di ip %s port %d" % (webserveraddr,port)
 SocketServer.TCPServer(("", port), Server).serve_forever()
